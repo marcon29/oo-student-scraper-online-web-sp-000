55245afe6379b68eec2f78cb8f02a0b176bb4f3f
Mark Stabler <mark@markstabler.com>
2020-04-02 00:01:26 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,11 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    # scrape index page
+    doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/index"))
+    # return array of hashes, each hash is one student
+    # each student hash contains name, location, profile url
+    # profile url will be used for 2nd scrape to get student details
   end
 
   def self.scrape_profile_page(profile_url)
